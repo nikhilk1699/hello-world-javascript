f86631b43fd047784553e461f7217706c19ec91f
nikhilk1699 <109533285+nikhilk1699@users.noreply.github.com>
2023-12-07 15:19:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//this task 2
+//task 2
 requrire('dotenv').config();
 const express = require('express');
 const app = express();
@@ -6,7 +6,7 @@ const app = express();
 const ipAddress = process.env.IP_ADDRESS || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-//this for taks 1
+//task 1
 //const ipAddress = '0.0.0.0';
 //const port = 3000;
 
